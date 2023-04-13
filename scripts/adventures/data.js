c495771ecb86afebfac8b46081df3aa89442da28
suzannahurst <hurstsuzanna@gmail.com>
2023-04-13 10:23:18 +0100

    removed dead markers temp

--- scripts/adventures/data.js
+++ scripts/adventures/data.js
@@ -133,208 +133,208 @@ const adventures = [
       },
     },
   },
-  {
-    id: 3,
-    attributes: {
-      name: "Point 3",
-      location: null,
-      description: "Lorem ipsum",
-      createdAt: "",
-      updatedAt: "",
-      publishedAt: "",
-      slug: "loc-1",
-      keyinfo: "",
+  // {
+  //   id: 3,
+  //   attributes: {
+  //     name: "Point 3",
+  //     location: null,
+  //     description: "Lorem ipsum",
+  //     createdAt: "",
+  //     updatedAt: "",
+  //     publishedAt: "",
+  //     slug: "loc-1",
+  //     keyinfo: "",
 
-      gm: "https://goo.gl/maps/J4DgjiuDvpmKNsU57",
-      latitude: 53.40922,
-      longitude: -2.98086,
-      type: "families",
-      openingTime: "Time",
-      lastEntry: "Time",
-      startingInstruction: " ",
-      logistics:
-        "Don't forget your smartphone. This route includes audio content and is better enjoyed with headphones, but captions will also be available.",
-      tags: {
-        data: [
-          {
-            id: 13,
-            attributes: {
-              name: "Smartphone required",
-              slug: "smartphone-required",
-              createdAt: "2022-09-19T16:35:52.600Z",
-              updatedAt: "2022-09-19T17:01:08.337Z",
-              publishedAt: "2022-09-19T17:01:08.334Z",
-            },
-          },
-          {
-            id: 2,
-            attributes: {
-              name: "Step-free access",
-              slug: "step-free-access",
-              createdAt: "2022-08-28T21:55:04.578Z",
-              updatedAt: "2022-09-15T16:28:49.859Z",
-              publishedAt: "2022-08-28T21:55:05.469Z",
-            },
-          },
-          {
-            id: 12,
-            attributes: {
-              name: "Age Guidance 12+",
-              slug: "over-12-years",
-              createdAt: "2022-09-19T16:30:59.838Z",
-              updatedAt: "2022-09-19T16:31:02.692Z",
-              publishedAt: "2022-09-19T16:31:02.689Z",
-            },
-          },
-          {
-            id: 15,
-            attributes: {
-              name: "Toilets available",
-              slug: "toilets-available",
-              createdAt: "2022-10-10T19:44:22.228Z",
-              updatedAt: "2022-10-12T22:14:26.297Z",
-              publishedAt: "2022-10-12T22:14:26.294Z",
-            },
-          },
-        ],
-      },
-    },
-  },
-  {
-    id: 4,
-    attributes: {
-      name: "Point 4",
-      location: null,
-      description: "Lorem ipsum",
-      createdAt: "",
-      updatedAt: "",
-      publishedAt: "",
-      slug: "loc-1",
-      keyinfo: "",
+  //     gm: "https://goo.gl/maps/J4DgjiuDvpmKNsU57",
+  //     latitude: 53.40922,
+  //     longitude: -2.98086,
+  //     type: "families",
+  //     openingTime: "Time",
+  //     lastEntry: "Time",
+  //     startingInstruction: " ",
+  //     logistics:
+  //       "Don't forget your smartphone. This route includes audio content and is better enjoyed with headphones, but captions will also be available.",
+  //     tags: {
+  //       data: [
+  //         {
+  //           id: 13,
+  //           attributes: {
+  //             name: "Smartphone required",
+  //             slug: "smartphone-required",
+  //             createdAt: "2022-09-19T16:35:52.600Z",
+  //             updatedAt: "2022-09-19T17:01:08.337Z",
+  //             publishedAt: "2022-09-19T17:01:08.334Z",
+  //           },
+  //         },
+  //         {
+  //           id: 2,
+  //           attributes: {
+  //             name: "Step-free access",
+  //             slug: "step-free-access",
+  //             createdAt: "2022-08-28T21:55:04.578Z",
+  //             updatedAt: "2022-09-15T16:28:49.859Z",
+  //             publishedAt: "2022-08-28T21:55:05.469Z",
+  //           },
+  //         },
+  //         {
+  //           id: 12,
+  //           attributes: {
+  //             name: "Age Guidance 12+",
+  //             slug: "over-12-years",
+  //             createdAt: "2022-09-19T16:30:59.838Z",
+  //             updatedAt: "2022-09-19T16:31:02.692Z",
+  //             publishedAt: "2022-09-19T16:31:02.689Z",
+  //           },
+  //         },
+  //         {
+  //           id: 15,
+  //           attributes: {
+  //             name: "Toilets available",
+  //             slug: "toilets-available",
+  //             createdAt: "2022-10-10T19:44:22.228Z",
+  //             updatedAt: "2022-10-12T22:14:26.297Z",
+  //             publishedAt: "2022-10-12T22:14:26.294Z",
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   },
+  // },
+  // {
+  //   id: 4,
+  //   attributes: {
+  //     name: "Point 4",
+  //     location: null,
+  //     description: "Lorem ipsum",
+  //     createdAt: "",
+  //     updatedAt: "",
+  //     publishedAt: "",
+  //     slug: "loc-1",
+  //     keyinfo: "",
 
-      gm: "https://goo.gl/maps/J4DgjiuDvpmKNsU57",
-      latitude: 53.40884,
-      longitude: -2.98136,
-      type: "families",
-      openingTime: "Time",
-      lastEntry: "Time",
-      startingInstruction: " ",
-      logistics:
-        "Don't forget your smartphone. This route includes audio content and is better enjoyed with headphones, but captions will also be available.",
-      tags: {
-        data: [
-          {
-            id: 13,
-            attributes: {
-              name: "Smartphone required",
-              slug: "smartphone-required",
-              createdAt: "2022-09-19T16:35:52.600Z",
-              updatedAt: "2022-09-19T17:01:08.337Z",
-              publishedAt: "2022-09-19T17:01:08.334Z",
-            },
-          },
-          {
-            id: 2,
-            attributes: {
-              name: "Step-free access",
-              slug: "step-free-access",
-              createdAt: "2022-08-28T21:55:04.578Z",
-              updatedAt: "2022-09-15T16:28:49.859Z",
-              publishedAt: "2022-08-28T21:55:05.469Z",
-            },
-          },
-          {
-            id: 12,
-            attributes: {
-              name: "Age Guidance 12+",
-              slug: "over-12-years",
-              createdAt: "2022-09-19T16:30:59.838Z",
-              updatedAt: "2022-09-19T16:31:02.692Z",
-              publishedAt: "2022-09-19T16:31:02.689Z",
-            },
-          },
-          {
-            id: 15,
-            attributes: {
-              name: "Toilets available",
-              slug: "toilets-available",
-              createdAt: "2022-10-10T19:44:22.228Z",
-              updatedAt: "2022-10-12T22:14:26.297Z",
-              publishedAt: "2022-10-12T22:14:26.294Z",
-            },
-          },
-        ],
-      },
-    },
-  },
+  //     gm: "https://goo.gl/maps/J4DgjiuDvpmKNsU57",
+  //     latitude: 53.40884,
+  //     longitude: -2.98136,
+  //     type: "families",
+  //     openingTime: "Time",
+  //     lastEntry: "Time",
+  //     startingInstruction: " ",
+  //     logistics:
+  //       "Don't forget your smartphone. This route includes audio content and is better enjoyed with headphones, but captions will also be available.",
+  //     tags: {
+  //       data: [
+  //         {
+  //           id: 13,
+  //           attributes: {
+  //             name: "Smartphone required",
+  //             slug: "smartphone-required",
+  //             createdAt: "2022-09-19T16:35:52.600Z",
+  //             updatedAt: "2022-09-19T17:01:08.337Z",
+  //             publishedAt: "2022-09-19T17:01:08.334Z",
+  //           },
+  //         },
+  //         {
+  //           id: 2,
+  //           attributes: {
+  //             name: "Step-free access",
+  //             slug: "step-free-access",
+  //             createdAt: "2022-08-28T21:55:04.578Z",
+  //             updatedAt: "2022-09-15T16:28:49.859Z",
+  //             publishedAt: "2022-08-28T21:55:05.469Z",
+  //           },
+  //         },
+  //         {
+  //           id: 12,
+  //           attributes: {
+  //             name: "Age Guidance 12+",
+  //             slug: "over-12-years",
+  //             createdAt: "2022-09-19T16:30:59.838Z",
+  //             updatedAt: "2022-09-19T16:31:02.692Z",
+  //             publishedAt: "2022-09-19T16:31:02.689Z",
+  //           },
+  //         },
+  //         {
+  //           id: 15,
+  //           attributes: {
+  //             name: "Toilets available",
+  //             slug: "toilets-available",
+  //             createdAt: "2022-10-10T19:44:22.228Z",
+  //             updatedAt: "2022-10-12T22:14:26.297Z",
+  //             publishedAt: "2022-10-12T22:14:26.294Z",
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   },
+  // },
 
-  {
-    id: 5,
-    attributes: {
-      name: "Point 5",
-      location: null,
-      description: "Lorem ipsum",
-      createdAt: "",
-      updatedAt: "",
-      publishedAt: "",
-      slug: "loc-1",
-      keyinfo: "",
+  // {
+  //   id: 5,
+  //   attributes: {
+  //     name: "Point 5",
+  //     location: null,
+  //     description: "Lorem ipsum",
+  //     createdAt: "",
+  //     updatedAt: "",
+  //     publishedAt: "",
+  //     slug: "loc-1",
+  //     keyinfo: "",
 
-      gm: "https://goo.gl/maps/J4DgjiuDvpmKNsU57",
-      latitude: 53.4089,
-      longitude: -2.98177,
-      type: "families",
-      openingTime: "Time",
-      lastEntry: "Time",
-      startingInstruction: " ",
-      logistics:
-        "Don't forget your smartphone. This route includes audio content and is better enjoyed with headphones, but captions will also be available.",
-      tags: {
-        data: [
-          {
-            id: 13,
-            attributes: {
-              name: "Smartphone required",
-              slug: "smartphone-required",
-              createdAt: "2022-09-19T16:35:52.600Z",
-              updatedAt: "2022-09-19T17:01:08.337Z",
-              publishedAt: "2022-09-19T17:01:08.334Z",
-            },
-          },
-          {
-            id: 2,
-            attributes: {
-              name: "Step-free access",
-              slug: "step-free-access",
-              createdAt: "2022-08-28T21:55:04.578Z",
-              updatedAt: "2022-09-15T16:28:49.859Z",
-              publishedAt: "2022-08-28T21:55:05.469Z",
-            },
-          },
-          {
-            id: 12,
-            attributes: {
-              name: "Age Guidance 12+",
-              slug: "over-12-years",
-              createdAt: "2022-09-19T16:30:59.838Z",
-              updatedAt: "2022-09-19T16:31:02.692Z",
-              publishedAt: "2022-09-19T16:31:02.689Z",
-            },
-          },
-          {
-            id: 15,
-            attributes: {
-              name: "Toilets available",
-              slug: "toilets-available",
-              createdAt: "2022-10-10T19:44:22.228Z",
-              updatedAt: "2022-10-12T22:14:26.297Z",
-              publishedAt: "2022-10-12T22:14:26.294Z",
-            },
-          },
-        ],
-      },
-    },
-  },
+  //     gm: "https://goo.gl/maps/J4DgjiuDvpmKNsU57",
+  //     latitude: 53.4089,
+  //     longitude: -2.98177,
+  //     type: "families",
+  //     openingTime: "Time",
+  //     lastEntry: "Time",
+  //     startingInstruction: " ",
+  //     logistics:
+  //       "Don't forget your smartphone. This route includes audio content and is better enjoyed with headphones, but captions will also be available.",
+  //     tags: {
+  //       data: [
+  //         {
+  //           id: 13,
+  //           attributes: {
+  //             name: "Smartphone required",
+  //             slug: "smartphone-required",
+  //             createdAt: "2022-09-19T16:35:52.600Z",
+  //             updatedAt: "2022-09-19T17:01:08.337Z",
+  //             publishedAt: "2022-09-19T17:01:08.334Z",
+  //           },
+  //         },
+  //         {
+  //           id: 2,
+  //           attributes: {
+  //             name: "Step-free access",
+  //             slug: "step-free-access",
+  //             createdAt: "2022-08-28T21:55:04.578Z",
+  //             updatedAt: "2022-09-15T16:28:49.859Z",
+  //             publishedAt: "2022-08-28T21:55:05.469Z",
+  //           },
+  //         },
+  //         {
+  //           id: 12,
+  //           attributes: {
+  //             name: "Age Guidance 12+",
+  //             slug: "over-12-years",
+  //             createdAt: "2022-09-19T16:30:59.838Z",
+  //             updatedAt: "2022-09-19T16:31:02.692Z",
+  //             publishedAt: "2022-09-19T16:31:02.689Z",
+  //           },
+  //         },
+  //         {
+  //           id: 15,
+  //           attributes: {
+  //             name: "Toilets available",
+  //             slug: "toilets-available",
+  //             createdAt: "2022-10-10T19:44:22.228Z",
+  //             updatedAt: "2022-10-12T22:14:26.297Z",
+  //             publishedAt: "2022-10-12T22:14:26.294Z",
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   },
+  // },
 ];
 
 export { adventures };
